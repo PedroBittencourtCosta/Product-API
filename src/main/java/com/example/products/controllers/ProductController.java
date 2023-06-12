ec50fadd82638a1d6e2efc086901c1b24d9730a8
PedroBittencourtCosta <Pedro.Bittencourt.dev@gmail.com>
2023-06-12 00:38:06 -0300

    Adicionando os novos endpoints de delete, put e get para um unico produto

--- src/main/java/com/example/products/controllers/ProductController.java
+++ src/main/java/com/example/products/controllers/ProductController.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -23,11 +25,42 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(service.getAll());
     }
 
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Object> getById(@PathVariable(value = "id") UUID id){
+        var product = service.getById(id);
+        if(product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
+
     @PostMapping("/product")
     public ResponseEntity<Product> postProduct(@RequestBody @Valid ProductDTO productDTO){
         Product product = new Product();
         BeanUtils.copyProperties(productDTO, product);
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.postProduct(product));
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveProduct(product));
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<Object> alterProduct(@PathVariable(value = "id") UUID id,
+                                       @RequestBody @Valid ProductDTO dto){
+        Optional<Product> productOptional = service.getById(id);
+        if(productOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("not found");
+        }
+        var product = productOptional.get();
+        BeanUtils.copyProperties(dto, product);
+        return ResponseEntity.status(HttpStatus.OK).body(service.saveProduct(product));
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id){
+        Optional<Product> productOptional = service.getById(id);
+        if (productOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found");
+        }
+        service.deleteProduct(productOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
     }
 
 
--- src/main/java/com/example/products/service/ProductService.java
+++ src/main/java/com/example/products/service/ProductService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
@@ -16,13 +18,26 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Transactional
     public List<Product> getAll(){
         List<Product> products = repository.findAll();
         return products;
     }
 
     @Transactional
-    public Product postProduct(Product product){
+    public Optional<Product> getById(UUID id){
+        Optional<Product> product = repository.findById(id);
+        return product;
+    }
+
+    @Transactional
+    public Product saveProduct(Product product){
         return repository.save(product);
     }
+
+    @Transactional
+    public void deleteProduct(Product product){
+        repository.delete(product);
+    }
+
 }
